751ebcf5e60788665321e8eea32b79d7b692e072
duyluong89 <duyluongnd@gmail.com>
2013-01-07 07:11:26 +0700

    add payment

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost:8080/base/';
+$config['base_url']	= 'http://localhost:8080/basecodeigniter/';
 
 /*
 |--------------------------------------------------------------------------
